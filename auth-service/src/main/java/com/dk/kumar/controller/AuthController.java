6457911c4adc92266eab962caf65ec3f0f3582b7
Durgesh <kdurgesh029@gmail.com>
2025-06-27 16:51:01 +0530

    add missed files

---
+++ auth-service/src/main/java/com/dk/kumar/controller/AuthController.java
@@ -0,0 +1,21 @@
+package com.dk.kumar.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+    @GetMapping("/")
+    public ResponseEntity<String> sayHii() {
+        return ResponseEntity.ok("Hello from user-service!");
+    }
+
+    @GetMapping("/hello")
+    public ResponseEntity<String> sayHello() {
+        return ResponseEntity.ok("Hello from user-service!");
+    }
+}
+
